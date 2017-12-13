55da3a605942e00beef44d5c59c28c41a66d3313
Bow225 <bowpawisuda2538@gmail.com>
2017-12-13 11:45:25 +0700

    sortingarray

---
+++ src/ooplab5/sortingArray.java
@@ -0,0 +1,70 @@
+package ooplab5;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class sortingArray {
+    private static final int MAX = 10;
+    private static int number[] = new int [MAX];
+    public static void main(String[] args) {
+        inputData(number,number.length);
+        System.out.println("Before sorting: ");
+        showData(number,number.length);
+        findMaxData(number);
+        // findMinData(number);
+        // sortingData(number);
+
+        sortingDataAscending(number);
+
+
+    }//main
+
+    private static void sortingData(int[] number) {
+    }//sortingdata
+
+    private static void findMinData(int[] number) {
+        System.out.print("Desending Order: ");
+        Arrays.sort(number, Collections.reverse(););
+        showData(number,number.length);
+    }//findmindata
+
+    private static void findMaxData(int[] number) {
+        int min = number[0];
+        for (int i=0;i<number.length;i++){
+            if (max<=number[i])
+                max = number[i];
+        }//for
+        System.out.println("The maximum data: " + min);
+
+    }//findmaxdata
+
+    private static void showData(int[] number, int length) {
+        System.out.print("Data in array: ");
+        for (int i=0;i<length;i++){
+            System.out.print(number[i]+" ");
+        }
+        System.out.println();
+    }
+
+
+    private static void inputData(int[] number, int length) {
+        //user input data
+        Scanner scanner = new Scanner(System.in);
+        for (int i=0;i<length;i++)
+        {
+            System.out.println("Enter an integer["+i+"]: ");
+            number[i] = scanner.nextInt();
+        }
+
+    }//inputData
+}//class
+
+
+
+
+
+
+
+
