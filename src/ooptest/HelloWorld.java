0b5c6521a9a2d2a8535adcd7841d7f5341713402
Bow225 <bowpawisuda2538@gmail.com>
2018-01-23 23:38:32 +0700

    HelloWorld

---
+++ src/ooptest/HelloWorld.java
@@ -0,0 +1,15 @@
+package ooptest;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world, I'm SAM");
+//        Add Data
+        System.out.println("Puriwat Lertkrai");
+        System.out.println("Information System");
+        System.out.println("Faculty of Management Technology,RMUTSV");
+
+    }//main
+
+}// class
