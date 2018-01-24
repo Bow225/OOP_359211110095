c4253ee0850319bcfa53edfd746ca9eaf689c921
Bow225 <bowpawisuda2538@gmail.com>
2018-01-23 20:23:22 -0800

    midterm31

--- src/ooplab5/test.java
+++
@@ -1,9 +0,0 @@
-package ooplab5;
-
-public class test {
-    private static final int MAX = 20;
-    private static int number[] = new int [MAX];
-    public static void main(String[] args) {
-        showData(number,number.length);
-
-    }
